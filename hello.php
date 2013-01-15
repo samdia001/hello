b0a30bbe2f96f34972cbdba1562ff3b5d49cf488
Mikael Roos <me@mikaelroos.se>
2013-01-15 14:29:24 +0100

    Major change

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 	<title>Hello World</title>
 </head>
 <body>
-<h1>Hello, new version is upcoming</h1>
+<h1>Hello, new version is upcoming, version 3</h1>
 <?php echo "My name is Mega Mic"; ?>
 </body>
 </html>
